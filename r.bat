3427f0438ca9dad6795f27553b6a5015751d018e
capt0-lll <evanin2003@gmail.com>
2026-06-14 19:32:19 +0300

    removed redundant script

--- r.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Repo-local Flutter runner: uses ..\..\flutter\bin\flutter.bat
-setlocal
-set SCRIPT_DIR=%~dp0
-"%SCRIPT_DIR%..\..\flutter\bin\flutter.bat" %*
-endlocal
-exit /b %ERRORLEVEL%
